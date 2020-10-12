89ef1000c4eea4b0cce389a1f7468de70a80edb3
Jackson Oppenheim <jacksonop@gmail.com>
2020-10-11 20:50:01 -0500

    king can climb

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 {
     // Config vars
     [SerializeField] float movementSpeed = 5f;
+    [SerializeField] float climbSpeed = 3f;
     [SerializeField] float jumpHeight = 5f;
     // state
 
@@ -13,6 +14,7 @@ public class Player : MonoBehaviour
     Rigidbody2D rigidbody2D;
     Animator animator;
     Collider2D collider2D;
+    float initialGravityScale;
 
 
     // Start is called before the first frame update
@@ -21,25 +23,35 @@ public class Player : MonoBehaviour
         rigidbody2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         collider2D = GetComponent<Collider2D>();
+        initialGravityScale = rigidbody2D.gravityScale;
     }
 
     // Update is called once per frame
     void Update ()
     {
-        Run();
-        Jump();
         ClimbLadder();
+        Jump();
+        Run();
+        FlipSprite();
     }
 
     private void ClimbLadder()
     {
         var isTouchingLadder = collider2D.IsTouchingLayers(LayerMask.GetMask("Ladder"));
-        if (isTouchingLadder)
+        if (!isTouchingLadder)
         {
-            var deltaY = Input.GetAxis("Vertical") * movementSpeed;
-            Vector2 playerClimbVelocity = new Vector2(rigidbody2D.velocity.x, deltaY);
-            rigidbody2D.velocity = playerClimbVelocity;
+            animator.SetBool("isClimbing", false);
+            rigidbody2D.gravityScale = initialGravityScale;
+            return;
         }
+        var deltaY = Input.GetAxis("Vertical") * climbSpeed;
+        Vector2 playerClimbVelocity = new Vector2(rigidbody2D.velocity.x, deltaY);
+        rigidbody2D.velocity = playerClimbVelocity;
+        rigidbody2D.gravityScale = 0;
+
+
+        var playerHasVertSpeed = Mathf.Abs(rigidbody2D.velocity.y) > Mathf.Epsilon;
+        animator.SetBool("isClimbing", playerHasVertSpeed);
     }
 
     private void Jump()
@@ -53,7 +65,7 @@ public class Player : MonoBehaviour
         if (Input.GetButtonDown("Jump"))
         {
             Vector2 jumpVelocity = new Vector2(0f, jumpHeight);
-            rigidbody2D.velocity = jumpVelocity;
+            rigidbody2D.velocity += jumpVelocity;
         }
     }
 
@@ -61,19 +73,14 @@ public class Player : MonoBehaviour
     {
         // Move character
         var deltaX = Input.GetAxis("Horizontal") * movementSpeed;
-        Debug.Log(deltaX);
         Vector2 playerVelocity = new Vector2 (deltaX, rigidbody2D.velocity.y);
         rigidbody2D.velocity = playerVelocity;
 
-        // Flip sprite
-        FlipSprite ();
 
         // set running anim
         bool playerHasHorizSpeed = Mathf.Abs (rigidbody2D.velocity.x) > Mathf.Epsilon;
         animator.SetBool ("isRunning", playerHasHorizSpeed);
 
-        var isTouchingLadder = collider2D.IsTouchingLayers(LayerMask.GetMask("Ladder"));
-        animator.SetBool("isClimbing", isTouchingLadder);
     }
 
     private void FlipSprite ()
